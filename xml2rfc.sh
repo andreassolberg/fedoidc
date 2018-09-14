cdfcb1f13de32b1f58a5b6a5d9b25b3f3ef46017
Andreas <andreas.solberg@uninett.no>
2018-09-14 15:02:22 +0200

    Create build directory

--- xml2rfc.sh
+++ xml2rfc.sh
@@ -3,4 +3,5 @@
 #docker run -v $(pwd):/rfc -w /rfc paulej/rfctools xml2rfc draft/oidcfed.hf.xml --text -o oidcfed-05.txt
 docker run -v $(pwd):/rfc -w /rfc paulej/rfctools xml2rfc draft/oidcfed.hf.xml --html -o oidcfed-05.html
 
+mkdir build
 mv oidcfed-05.html build/
