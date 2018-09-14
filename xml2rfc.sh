cac45ca73459dbedfa17337702ec51144f8dd457
Andreas <andreas.solberg@uninett.no>
2018-09-14 14:56:08 +0200

    Add GCS build using travisbuild using travis

---
+++ xml2rfc.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#docker run -v $(pwd):/rfc -w /rfc paulej/rfctools xml2rfc draft/oidcfed.hf.xml --text -o oidcfed-05.txt
+docker run -v $(pwd):/rfc -w /rfc paulej/rfctools xml2rfc draft/oidcfed.hf.xml --html -o oidcfed-05.html
+
+mv oidcfed-05.html build/
